b8f7c9baecb8f506fb68fe048fe0fd29d9fcf425
David_Hernandez <julio-david@hotmail.com>
2022-09-27 21:19:16 +0000

    Adding allauth app

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/',include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls')
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
