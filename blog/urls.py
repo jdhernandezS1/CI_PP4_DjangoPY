0e093c02c4250a1fdaae1234a3951a6f9a5e2dbc
David_Hernandez <julio-david@hotmail.com>
2022-10-02 20:01:18 +0000

    add count likes

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index.as_view(), name='home'),
     path('posts/', views.PostList.as_view(), name='posts'),
     path('posts/<slug:slug>/', views.PostDetail.as_view(), name="post_detail"),
+    path('posts/<slug:slug>/like', views.PostLike.as_view(), name="post_like"),
 ]
--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -53,6 +54,14 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
