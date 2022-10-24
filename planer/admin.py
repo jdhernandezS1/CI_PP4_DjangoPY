b1124a2237b67dda6c2919e6f78bba45ace7b65c
David_Hernandez <julio-david@hotmail.com>
2022-10-24 16:08:22 +0000

    setting database structure

--- planer/admin.py
+++ planer/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+
+@admin.register(Week)
+class WeekAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'weeknumber', 'created_on')
+    search_fields = ['title', 'weeknumber']
+    list_filter = ('title', 'created_on')
+    
+
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('day_number', 'created_on')
+    list_filter = ('day_name', 'created_on')
+    search_fields = ('day_name', 'day_number')
+    
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'meal_number', 'meal_name', 'created_on')
+    list_filter = ('meal_number', 'meal_name', 'created_on')
+    search_fields = ('meal_number', 'meal_name')
--- planer/models.py
+++ planer/models.py
@@ -1,36 +1,69 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-# from datetime import timedelta
+# import datetime
+
 
 STATUS = ((0, "DRAFT"), (1, "Published"))
 
 
 class Week(models.Model):
-    title = models.CharField(max_length=50, unique=True)
+    """
+    A class for the weekly meals
+    """
+
+    title = models.CharField(max_length=15, unique=True)
     period = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE,  related_name="week_plan")
+    weeknumber = models.ForeignKey(User, on_delete=models.CASCADE, related_name="week")
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
+    featured_image = CloudinaryField('image', default='placeholder')
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['-created_on', 'weeknumber']
 
     def __str__(self):
         return self.title
 
 
 class Day(models.Model):
+    """
+    A class for the daily meals
+    """
 
-    post = models.ForeignKey(Week, on_delete=models.CASCADE, related_name='days')
-    day_name = models.CharField(max_length=12)
-    body = models.TextField()
+    day_name = models.CharField(max_length=9, unique=True)
+    day_number = models.ForeignKey(Week, on_delete=models.CASCADE, related_name='day')
+    phrase = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=True)
     featured_image = CloudinaryField('image', default='placeholder')
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['day_number', 'created_on']
 
+
+class Meal(models.Model):
+    """
+    A class for the individual meals
+    """
+
+    meal = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='meal')
+    meal_number = models.DecimalField(decimal_places=0,max_digits=1, unique=True)
+    meal_name = models.CharField(max_length=30)
+    meal_description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['meal_number', 'created_on']
+    
+    def __str__(self):
+        """
+        Returns the meal string
+        """
+        return self.meal_name
+
+#// COMMANDS TIME
+#// save the act day
+#  todays_date = datetime.date.today()
+#// get the # of the day in the week
+# day = todays_date.isoweekday()
