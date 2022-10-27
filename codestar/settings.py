a1314ca32005e5d821dac2cc66a03c0defc20976
David_Hernandez <julio-david@hotmail.com>
2022-10-27 14:37:08 +0000

    Frame options and allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
+ALLOWED_HOSTS = ['djangoapp4.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
