1b48616f42e7ce62300a7c60778cd57fdd24b976
David_Hernandez <julio-david@hotmail.com>
2022-11-07 18:52:47 +0000

    planer setting views

--- planer/urls.py
+++ planer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('planer/', views.planer_index.as_view(), name='planer'),
+    path('planer/', views.PlanerList.as_view(), name='planer'),
     # path('planer/week/', views.WeekList.as_view(), name='week_list'),
     # path('planer/day/', views.WeekDetail.as_view(), name="days_list"),
     # path('planer/meals/', views.Meals.as_view(), name="meals_list"),
--- planer/views.py
+++ planer/views.py
@@ -4,15 +4,16 @@ from django.http import HttpResponseRedirect
 from .models import Week
 from .forms import *
 
-class planer_index(generic.ListView):
+class PlanerList(generic.ListView):
     """
     A class for the main page "planer index"
     """
     model = Week
+    queryset = Week.objects.order_by('-created_on')
     template_name = 'planer_index.html'
     paginate_by = 6
-
-
+    context = {'flag' : '0',} 
+    
 
 class WeekList(generic.ListView):
     """
@@ -20,5 +21,23 @@ class WeekList(generic.ListView):
     """
     model = Week
     queryset = Week.objects.filter(status=1).order_by('-created_on')
-    template_name = 'week.html'
+    template_name = 'planner_index.html'
     paginate_by = 6
+# hacer este modificar el request          
+# class PlanerDetail(View):
+#     """
+#     A class for the Post details ordered by "created on"
+#     """
+#     def get(self, request, title, *args, **kwargs):
+#         queryset = Week.objects.filter(status=1)
+#         # week = get_object_or_404(queryset, slug=slug)\
+#         return render(
+#             request,
+#             "post_detail.html",
+#             {
+#                 "post": post,
+#                 "comments": comments,
+#                 "liked": liked,
+#                 "comment_form": CommentForm()
+#             },
+#         )
