67a0b9690a63b731c964a6975f9e599a9ee65faf
David_Hernandez <julio-david@hotmail.com>
2022-11-13 18:38:19 +0000

    daily url

--- planer/urls.py
+++ planer/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('planer/week', views.PlanerList.as_view(), name='planer'),
-    path('planer/', views.PlanerDaily.as_view(), name='day'),
+    path('planer/', views.PlanerDaily.as_view(), name='planer'),
+    path('planer/week', views.PlanerList.as_view(), name='planer_week'),
     # path('planer/week/', views.WeekList.as_view(), name='week_list'),
     # path('planer/day/', views.WeekDetail.as_view(), name="days_list"),
     # path('planer/meals/', views.Meals.as_view(), name="meals_list"),
