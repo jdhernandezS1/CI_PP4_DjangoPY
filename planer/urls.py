66f75b0c7ef2a955480d66703b38aacbdd7e9c15
David_Hernandez <julio-david@hotmail.com>
2022-11-15 22:59:30 +0000

    conecting view urls

--- planer/urls.py
+++ planer/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('planer/', views.PlanerList.as_view(), name='planer'),
-    path('planer/day>', views.PlanerDaily.as_view(), name='planer_day'),
+    path('planer/<slug:slug>', views.PlanerDaily.as_view(), name='planer_day'),
     # path('planer/week/', views.WeekList.as_view(), name='week_list'),
     # path('planer/day/', views.WeekDetail.as_view(), name="days_list"),
     # path('planer/meals/', views.Meals.as_view(), name="meals_list"),
