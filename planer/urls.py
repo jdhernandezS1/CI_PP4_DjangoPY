20f619bac6900ec5bfe685458854afba467cc22d
David_Hernandez <julio-david@hotmail.com>
2022-11-16 22:49:07 +0000

    add Meals page

--- planer/urls.py
+++ planer/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('planer/', views.PlanerList.as_view(), name='planer'),
     path('planer/<slug:slug>', views.PlanerDaily.as_view(), name='planer_day'),
-    # path('planer/week/', views.WeekList.as_view(), name='week_list'),
+    path('planer/<slug:slug>/meals', views.Meals.as_view(), name='meals_list'),
     # path('planer/day/', views.WeekDetail.as_view(), name="days_list"),
     # path('planer/meals/', views.Meals.as_view(), name="meals_list"),
 ]
