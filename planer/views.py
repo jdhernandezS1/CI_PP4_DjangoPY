d32d78165fd3e98604932490706f2d09581a76fd
David_Hernandez <julio-david@hotmail.com>
2022-11-16 22:49:17 +0000

    add Meals view

--- planer/views.py
+++ planer/views.py
@@ -17,76 +17,37 @@ class PlanerDaily(generic.ListView):
     """
     A class for the daily planer "planer_day.html"
     """
-    # model = Day
-    # queryset = Day.objects.order_by('-created_on')
-    # template_name = 'planer_day.html'
     def get(self, request, slug, *args, **kwargs):
         queryset = Week.objects.filter(status=1)
         week = get_object_or_404(queryset, slug=slug)
-        days = week.day.order_by('day_name')
+        day = week.day.order_by('day_name')
         return render(
             request,
             "planer_day.html",
             {
                 "week": week,
-                "days": days,
+                "day": day,
                 "slug": slug
             },
         )
 
-class meals(generic.ListView):
+class Meals(generic.ListView):
     """
     A class for the Weeks ordered by "created on"
     """
-    model = Week
-    queryset = Week.objects.filter(status=1).order_by('-created_on')
-    template_name = 'planner_index.html'
-    paginate_by = 6
-    def post(self, request, slug, *args, **kwargs):
-        
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
-
+    # model = Meal
+    # queryset = Meal.objects.order_by('-created_on')
+    # template_name = 'planer_meal.html'
+    # paginate_by = 6
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Day.objects
+        day = get_object_or_404(queryset, slugday=slug)
+        meal = day.meal.order_by('meal_number')
         return render(
             request,
-            "post_detail.html",
+            "planer_meal.html",
             {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "comment_form": comment_form,
-                "liked": liked
+                "meal": meal,
+                "slug": slug
             },
         )
-# hacer este modificar el request          
-# class PlanerDetail(View):
-#     """
-#     A class for the Post details ordered by "created on"
-#     """
-#     def get(self, request, title, *args, **kwargs):
-#         queryset = Week.objects.filter(status=1)
-#         # week = get_object_or_404(queryset, slug=slug)\
-#         return render(
-#             request,
-#             "post_detail.html",
-#             {
-#                 "post": post,
-#                 "comments": comments,
-#                 "liked": liked,
-#                 "comment_form": CommentForm()
-#             },
-#         )
