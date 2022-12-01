41f844a75cc98e00918b6ed6f7702757535a659c
David_Hernandez <julio-david@hotmail.com>
2022-12-01 13:07:23 +0000

    Fix Wave Performance

--- blog/views.py
+++ blog/views.py
@@ -54,6 +54,8 @@ class PostDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
         context = {
                 "post": post,
                 "commented": True,
@@ -61,8 +63,6 @@ class PostDetail(View):
                 "liked": liked,
                 "comment_form": CommentForm()
             }
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
         return render(
             request,
             "post_detail.html",
