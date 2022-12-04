9d0da092837da695343500155b10ead7d469997b
David_Hernandez <julio-david@hotmail.com>
2022-12-04 13:17:46 +0000

    DataBases change to test

--- codestar/settings.py
+++ codestar/settings.py
@@ -117,8 +117,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
+#         }
 #     }
-# }
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
